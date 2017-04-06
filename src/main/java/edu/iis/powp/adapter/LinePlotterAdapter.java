99abfc43209c473b41d014775467fbd40719f046
wierzynski.patryk <p.wierzynski@live.com>
2017-04-06 12:59:26 +0200

    added LinePlotterAdapter

---
+++ src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
@@ -0,0 +1,48 @@
+package edu.iis.powp.adapter;
+
+import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.app.Context;
+import edu.kis.powp.drawer.panel.DrawPanelController;
+import edu.kis.powp.drawer.shape.ILine;
+import edu.kis.powp.drawer.shape.LineFactory;
+
+/**
+ * Created by Patryk Wierzyński
+ */
+public class LinePlotterAdapter implements IPlotter {
+
+	private DrawPanelController controller;
+	private int startX = 0, startY = 0;
+	private ILine line;
+
+	public LinePlotterAdapter(DrawPanelController controller, ILine line) {
+		this.controller = controller;
+		this.line = line;
+	}
+
+	@Override
+	public void setPosition(int x, int y)
+	{
+		this.startX = x;
+		this.startY = y;
+	}
+
+	@Override
+	public void drawTo(int x, int y)
+	{
+		line = LineFactory.getBasicLine();
+		line.setStartCoordinates(this.startX, this.startY);
+		line.setEndCoordinates(x, y);
+
+		controller.drawLine(line);
+		setPosition(x, y);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Line Simulator";
+	}
+
+
+}
